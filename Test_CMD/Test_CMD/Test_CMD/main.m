c20f749a4ae02c01abb6db8310e4ef1f826cc650
ChrisA <grommett@me.com>
2014-08-30 00:15:06 +1000

    Initial Commit

---
+++ Test_CMD/Test_CMD/Test_CMD/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Test_CMD
+//
+//  Created by Chris Anderson on 30/08/2014.
+//  Copyright (c) 2014 Chris Anderson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
